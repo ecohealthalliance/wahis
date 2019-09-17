c8e3742ee2362796e98815cba9ebd2d6b7217381
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-09-17 09:55:03 -0400

    Add disease absence tables

--- inst/processing_scripts/wahis_pull_reports.R
+++ inst/processing_scripts/wahis_pull_reports.R
@@ -2,5 +2,5 @@ library(pbapply)
 devtools::load_all()
 filenames <- list.files("data-raw/raw_wahis_reports",
                         pattern = "*.html",
-                        full.names = TRUE)[11]
-wahis <- pblapply(filenames, ingest_wahis_report, cl=40)  ## 25448 files read
+                        full.names = TRUE)[11:12]
+wahis <- pblapply(filenames, ingest_wahis_report, cl=40)  
