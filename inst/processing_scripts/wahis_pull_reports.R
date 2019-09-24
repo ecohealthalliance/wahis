c28ef1600fdb388d058fcf9a1cd91e993b1507aa
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-09-24 10:50:30 -0400

    Handling missing tables

--- inst/processing_scripts/wahis_pull_reports.R
+++ inst/processing_scripts/wahis_pull_reports.R
@@ -1,7 +1,13 @@
-library(pbapply)
+library(pbapply) 
 devtools::load_all()
 filenames <- list.files("data-raw/raw_wahis_reports",
                         pattern = "*.html",
                         full.names = TRUE)[11:12]
-filenames <- "data-raw/raw_wahis_reports/USA_2017_sem0.html"
+filenames <- c("data-raw/raw_wahis_reports/USA_2017_sem0.html",
+               "data-raw/raw_wahis_reports/USA_2016_sem0.html",
+               "data-raw/raw_wahis_reports/AND_2016_sem0.html",
+               "data-raw/raw_wahis_reports/ARM_2016_sem0.html",
+               "data-raw/raw_wahis_reports/YEM_2016_sem0.html"               )
+
+
 wahis <- pblapply(filenames, ingest_wahis_report, cl=40)  
