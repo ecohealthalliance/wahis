0c2250fbc3e852cf8e9e2abf8eaf6d6021a25193
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-10-02 11:34:54 -0400

    Semester reporting

--- inst/processing_scripts/wahis_pull_reports.R
+++ inst/processing_scripts/wahis_pull_reports.R
@@ -3,8 +3,7 @@ devtools::load_all()
 filenames <- list.files("data-raw/raw_wahis_reports",
                         pattern = "*.html",
                         full.names = TRUE)
-filenames <- filenames[grepl("2014_sem0", filenames)]
-filenames <- filenames[sample(length(filenames), 100)]
+filenames <- filenames[sample(length(filenames), 200)]
 
 #wahis <- pblapply(filenames, ingest_wahis_report, cl=40)  
 
@@ -15,16 +14,4 @@ for(i in seq_along(filenames)){
 }
 
 web_page = filenames[[i]]
-web_page = "data-raw/raw_wahis_reports/USA_2012_sem0.html"
-
-diseases <- map_df(wahis, function(x){
-    if(x=="report error"){return()}
-    x$diseases_present %>%
-        filter(!is.na(serotype_s), !serotype_s %in% c("...", "No")) %>%
-        mutate(country = x$country) %>%
-        select(country, everything()) %>%
-        arrange(disease)
-})
-nrow(diseases)
-
-diseases <- diseases %>% arrange(disease)
+web_page = "data-raw/raw_wahis_reports/USA_2016_sem1.html"
