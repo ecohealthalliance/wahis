59b054eb253ddacef877838c9c959026d631e658
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-10-02 09:07:52 -0400

    General recoding

--- inst/processing_scripts/wahis_pull_reports.R
+++ inst/processing_scripts/wahis_pull_reports.R
@@ -3,7 +3,8 @@ devtools::load_all()
 filenames <- list.files("data-raw/raw_wahis_reports",
                         pattern = "*.html",
                         full.names = TRUE)
-filenames <- filenames[grepl("2012_sem0", filenames)][1:2]
+filenames <- filenames[grepl("2014_sem0", filenames)]
+filenames <- filenames[sample(length(filenames), 100)]
 
 #wahis <- pblapply(filenames, ingest_wahis_report, cl=40)  
 
@@ -14,4 +15,16 @@ for(i in seq_along(filenames)){
 }
 
 web_page = filenames[[i]]
-web_page = "data-raw/raw_wahis_reports/USA_2017_sem0.html"
+web_page = "data-raw/raw_wahis_reports/USA_2012_sem0.html"
+
+diseases <- map_df(wahis, function(x){
+    if(x=="report error"){return()}
+    x$diseases_present %>%
+        filter(!is.na(serotype_s), !serotype_s %in% c("...", "No")) %>%
+        mutate(country = x$country) %>%
+        select(country, everything()) %>%
+        arrange(disease)
+})
+nrow(diseases)
+
+diseases <- diseases %>% arrange(disease)
