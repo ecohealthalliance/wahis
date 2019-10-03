61de549151869eb98e9c00adf3f5761ce0c1a414
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2019-10-03 10:02:37 -0400

    Full data download and upload to AWS

--- inst/processing_scripts/wahis_pull_reports.R
+++ inst/processing_scripts/wahis_pull_reports.R
@@ -3,15 +3,42 @@ devtools::load_all()
 filenames <- list.files("data-raw/raw_wahis_reports",
                         pattern = "*.html",
                         full.names = TRUE)
-filenames <- filenames[sample(length(filenames), 200)]
+# filenames <- filenames[sample(length(filenames), 200)] # for testing data subset
 
-#wahis <- pblapply(filenames, ingest_wahis_report, cl=40)  
+wahis <- pblapply(filenames, safe_ingest, cl=40)  
+# readr::write_rds(wahis, here::here("data", "wahis.rds"))
 
-wahis <- list()
-for(i in seq_along(filenames)){
-    out <- ingest_wahis_report(filenames[[i]])
-    wahis[[i]] <- out
-}
+# to retrieve from AWS:
+library(aws.s3)
+aws.signature::use_credentials()
+Sys.setenv("AWS_DEFAULT_REGION" = "us-east-1")
 
-web_page = filenames[[i]]
-web_page = "data-raw/raw_wahis_reports/USA_2016_sem1.html"
+save_object(object = "wahis-data.tar.xz", 
+            bucket = "wahis-data",
+            file = here::here("data", "wahis-data.tar.xz"),
+            overwrite = TRUE)
+untar(here::here("data", "wahis-data.tar.xz"), exdir = ".", tar = "internal")
+
+# Post to AWS S3
+# See https://ecohealthalliance.github.io/eha-ma-handbook/11-cloud-computing-services.html
+# For credentials setup
+# library(aws.s3)
+# aws.signature::use_credentials()
+# Sys.setenv("AWS_DEFAULT_REGION" = "us-east-1")
+
+# Create the bucket, only done once
+# aws.s3::put_bucket(bucket = "wahis-data", acl = "private")
+
+# To deposit updated scraped data
+# Compress the data-raw folder
+# tar("wahis-data.tar.xz",
+#     files = "data",
+#     compression = "xz",      # xz and level 9 makes this slow, but small!
+#     compression_level = 9,
+#     tar = "internal")
+# Upload the compressed file
+# put_object(file = "wahis-data.tar.xz",
+#            object = "wahis-data.tar.xz",
+#            bucket = "wahis-data",
+#            multipart = TRUE,
+#            verbose = TRUE)
