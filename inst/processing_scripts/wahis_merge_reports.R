9d21a91be9f44e76b44fcadace94d706e92f2ad5
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2019-10-04 09:32:06 -0400

    Separate wahis syncing file

---
+++ inst/processing_scripts/wahis_merge_reports.R
@@ -0,0 +1,33 @@
+library(tidyverse)
+
+# Merge files -------------------------------------------------------------
+wahis <- readr::read_rds(here::here("data", "wahis.rds"))
+
+# Remove report errors
+wahis <- discard(wahis, function(x){
+    length(x) == 1
+})
+
+# Add country and report period to every table
+wahis <- purrr::map(wahis, function(x){
+    country <- x$metadata$country %>% unique()
+    report_year <- x$metadata$report_year %>% unique()
+    report_months <- x$metadata$report_months %>% unique()
+    purrr::map(x, function(y){
+        if(is.null(y)){return()}
+        y %>% mutate(country = country,
+                     report_year = report_year,
+                     report_months = report_months) %>%
+            select(country, report_year, report_months, everything())
+    })
+})
+
+# Extract and rbind tables
+wahis_joined <- map(names(wahis[[1]]), function(name){
+    map_dfr(wahis, ~magrittr::extract2(., name)) 
+})
+names(wahis_joined) <- names(wahis[[1]])
+
+# Save
+write_rds(wahis_joined, here::here("data", "wahis-merged.rds"))
+
--- inst/processing_scripts/wahis_pull_reports.R
+++ inst/processing_scripts/wahis_pull_reports.R
@@ -1,45 +1,16 @@
 library(pbapply) 
 library(wahis)
 #devtools::load_all()
+
+# Pull files --------------------------------------------------------------
+
+# List all file names
 filenames <- list.files(here::here("data-raw/raw_wahis_reports"),
                         pattern = "*.html",
                         full.names = TRUE)
 
+# Run scraper (~1 hr)
 wahis <- pblapply(filenames, safe_ingest, cl=40)  
+
+# Save
 readr::write_rds(wahis, here::here("data", "wahis.rds"))
-
-# Retrieve from AWS -------------------------------------------------------------
-# library(aws.s3)
-# aws.signature::use_credentials()
-# Sys.setenv("AWS_DEFAULT_REGION" = "us-east-1")
-# 
-# save_object(object = "wahis-data.tar.xz",
-#             bucket = "wahis-data",
-#             file = here::here("data", "wahis-data.tar.xz"),
-#             overwrite = TRUE)
-# untar(here::here("data", "wahis-data.tar.xz"), exdir = ".", tar = "internal")
-# wahis <- readr::read_rds(here::here("data", "wahis.rds"))
-
-# Post to AWS -------------------------------------------------------------
-# See https://ecohealthalliance.github.io/eha-ma-handbook/11-cloud-computing-services.html
-# For credentials setup
-# library(aws.s3)
-# aws.signature::use_credentials()
-# Sys.setenv("AWS_DEFAULT_REGION" = "us-east-1")
-
-# Create the bucket, only done once
-# aws.s3::put_bucket(bucket = "wahis-data", acl = "private")
-
-# To deposit updated scraped data
-# Compress the data folder
-# tar("wahis-data.tar.xz",
-#     files = "data/wahis.rds",
-#     compression = "xz",      # xz and level 9 makes this slow, but small!
-#     compression_level = 9,
-#     tar = "internal")
-# Upload the compressed file
-# put_object(file = "wahis-data.tar.xz",
-#            object = "wahis-data.tar.xz",
-#            bucket = "wahis-data",
-#            multipart = TRUE,
-#            verbose = TRUE)
