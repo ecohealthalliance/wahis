1af71fcf246458834ac262c6ce3efe4ddc063dd3
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2019-10-11 08:39:51 -0400

    Use furrr instead of pblapply for faster scheduling

--- inst/processing_scripts/process_annual_reports.R
+++ inst/processing_scripts/process_annual_reports.R
@@ -1,6 +1,9 @@
 #!/usr/bin/env Rscript
 
-library(pbapply) 
+library(furrr)
+library(future)
+library(fs)
+plan(multiprocess) # This takes a long time in RStudio, but is faster from terminal
 devtools::load_all(here::here()) #doing this as scraping functions may not be exported
 
 # List all files  ---------------------------------------------------------
@@ -10,8 +13,8 @@ filenames <- list.files(here::here("data-raw/wahis_raw_annual_reports"),
 
 # Run scraper (~1 hr) ---------------------------------------------------------
 message(paste(length(filenames), "files to process"))
-opb <- pboptions(type="timer")
-wahis <- pblapply(filenames, wahis:::safe_ingest, cl = parallel::detectCores())  
+wahis <- future_map(filenames, wahis:::safe_ingest, .progress = TRUE)  
 
 # Save processed files   ------------------------------------------------------
+dir_create(here::here("data-processed"))
 readr::write_rds(wahis, here::here("data-processed", "processed-annual-reports.rds"), compress = "xz", compression = 9L)
