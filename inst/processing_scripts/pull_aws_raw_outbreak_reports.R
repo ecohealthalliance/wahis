fb5a4416cd60b0005c4903a6e561e3308e20e85e
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2019-10-22 13:53:55 -0400

    update aws pull/push for outbreak reports

--- inst/processing_scripts/pull_aws_raw_outbreak_reports.R
+++ inst/processing_scripts/pull_aws_raw_outbreak_reports.R
@@ -3,25 +3,7 @@
 # Script to sync data files
 # See https://ecohealthalliance.github.io/eha-ma-handbook/11-cloud-computing-services.html
 # For credentials setup
-library(aws.s3)
-if (Sys.getenv("CI_JOB_ID") != "") {
-    aws.signature::use_credentials(file = Sys.getenv("AWS_SIGNATURE_PATH"))
-} else {
-    aws.signature::use_credentials()
-}
-Sys.setenv("AWS_DEFAULT_REGION" = "us-east-1")
 
+wahis:::pull_aws(bucket = "wahis-data", object = "wahis-raw-outbreak-reports.tar.xz", dir = ".")
 
-save_object(object = "wahis-raw-outbreak-reports.tar.xz", 
-            bucket = "wahis-data",
-            file = "wahis-raw-outbreak-reports.tar.xz",
-            overwrite = TRUE)
-untar("wahis-raw-outbreak-reports.tar.xz", exdir = "data-raw", tar = "internal")
-file.remove("wahis-raw-outbreak-reports.tar.xz")
-
-save_object(object = "wahis-raw-outbreak-pdfs.tar.xz", 
-            bucket = "wahis-data",
-            file = "wahis-raw-outbreak-pdfs.tar.xz",
-            overwrite = TRUE)
-untar("wahis-raw-outbreak-pdfs.tar.xz", exdir = "data-raw", tar = "internal")
-file.remove("wahis-raw-outbreak-pdfs.tar.xz")
+wahis:::pull_aws(bucket = "wahis-data", object = "wahis-raw-outbreak-pdfs.tar.xz", dir = ".")
--- inst/processing_scripts/push_aws_raw_outbreak_reports.R
+++ inst/processing_scripts/push_aws_raw_outbreak_reports.R
@@ -3,41 +3,7 @@
 # Script to sync data files
 # See https://ecohealthalliance.github.io/eha-ma-handbook/11-cloud-computing-services.html
 # For credentials setup
-library(aws.s3)
-if (Sys.getenv("CI_JOB_ID") != "") {
-    aws.signature::use_credentials(file = Sys.getenv("AWS_SIGNATURE_PATH"))
-} else {
-    aws.signature::use_credentials()
-}
-Sys.setenv("AWS_DEFAULT_REGION" = "us-east-1")
 
+wahis:::push_aws(folder = "data-raw/wahis-raw-outbreak-reports", bucket = "wahis-data")
 
-if (!bucket_exists("wahis-data")) {
-    aws.s3::put_bucket(bucket = "wahis-data", acl = "private")
-}
-
-#Compress the raw data 
-setwd(here::here("data-raw"))
-tar("wahis-raw-outbreak-reports.tar.xz",
-    files = "wahis_raw_outbreak_reports",
-    compression = "xz",      # xz and level 9 makes this slow, but small!
-    compression_level = 9,
-    tar = "internal")
-# Upload the compressed file
-put_object(file = "wahis-raw-outbreak-reports.tar.xz",
-           object = "wahis-raw-outbreak-reports.tar.xz",
-           bucket = "wahis-data",
-           multipart = FALSE,
-           verbose = FALSE)
-tar("wahis-raw-outbreak-pdfs.tar.xz",
-    files = "wahis_raw_outbreak_pdfs",
-    compression = "xz",      # xz and level 9 makes this slow, but small!
-    compression_level = 9,
-    tar = "internal")
-# Upload the compressed file
-put_object(file = "wahis-raw-outbreak-pdfs.tar.xz",
-           object = "wahis-raw-outbreak-pdfs.tar.xz",
-           bucket = "wahis-data",
-           multipart = FALSE,
-           verbose = FALSE)
-setwd(here::here())
+wahis:::push_aws(folder = "data-raw/wahis-raw-outbreak-pdfs", bucket = "wahis-data")
