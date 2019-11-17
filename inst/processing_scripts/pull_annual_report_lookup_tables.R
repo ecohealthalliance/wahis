191c97f50bf732b05e7fd33d55b0b88859b15181
Noam Ross (Travis-CI) <ross@ecohealthalliance.org>
2019-11-17 17:51:23 -0500

    Pull lookup reports to `inst/`

--- inst/processing_scripts/pull_annual_report_lookup_tables.R
+++ inst/processing_scripts/pull_annual_report_lookup_tables.R
@@ -13,12 +13,12 @@ if (Sys.getenv("CI_JOB_ID") != "") {
 
 wb <- drive_download("https://docs.google.com/spreadsheets/d/18tgKfu-3oA_xycVO07LGMN7CIEZ1WYnMDz0ZF665zuo",
                      overwrite = TRUE)
-dir_create(here::here("data-raw", "annual_report_lookups"))
+dir_create(here::here("inst", "annual_report_lookups"))
 
 walk(excel_sheets(wb$local_path), function(z) {
     message(z)
     write_csv(read_excel(wb$local_path, z),
-              here::here("data-raw", "annual_report_lookups",
+              here::here("inst", "annual_report_lookups",
                          paste0(z, ".csv")))
 })
 file_delete(wb$local_path)
