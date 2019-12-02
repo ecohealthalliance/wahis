4338233ecab8dcee31e2b0a2e958112e4cb5a863
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2019-12-02 11:57:26 -0500

    add transform function to outbreak processing

--- inst/processing_scripts/process_outbreak_reports.R
+++ inst/processing_scripts/process_outbreak_reports.R
@@ -3,6 +3,8 @@
 library(fs)
 library(future)
 library(furrr)
+library(tidyverse)
+
 # Set up parallel plan  --------------------------------------------------------
 
 plan(multiprocess) # This takes a bit to load on many cores as all the processes are starting
@@ -13,7 +15,7 @@ filenames <- list.files(here::here("data-raw/wahis-raw-outbreak-reports"),
                         pattern = "*.html",
                         full.names = TRUE)
 
-# Run scraper (~25 mins) ---------------------------------------------------------
+# Run ingest (~25 mins) ---------------------------------------------------------
 message(paste(length(filenames), "files to process"))
 wahis_outbreak <- future_map(filenames, wahis:::safe_ingest_outbreak, .progress = TRUE)  
 
@@ -24,6 +26,20 @@ wahis_outbreak <- future_map(filenames, wahis:::safe_ingest_outbreak, .progress 
 # noamtools::proftable("out.prof")
 
 
-# Save processed files   ------------------------------------------------------
+# Save ingested files   ------------------------------------------------------
 dir_create(here::here("data-processed"))
 readr::write_rds(wahis_outbreak, here::here("data-processed", "processed-outbreak-reports.rds"), compress = "xz", compression = 9L)
+
+# Transform files   ------------------------------------------------------
+outbreak_reports <-  readr::read_rds(here::here("data-processed", "processed-outbreak-reports.rds"))
+
+outbreak_reports_transformed <- wahis::transform_annual_reports(outbreak_reports)
+
+# Export transformed files-----------------------------------------------
+dir_create( here::here("data-processed", "db"))
+purrr::iwalk(annual_reports_transformed, ~readr::write_csv(.x, here::here("data-processed", "db", paste0(.y, ".csv.xz"))))
+readr::write_csv(ingest_status_log, here::here("data-processed", "db", "annual_reports_ingest_status_log.csv.xz"))
+
+readr::write_rds(annual_reports_transformed, here::here("data-processed", "annual-reports-data.rds"), compress = "xz", compression = 9L)
+
+
