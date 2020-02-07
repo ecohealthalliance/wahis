1c5c1e8870c7c5b2fe48d2f714c5c7cc392dd0d2
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2020-02-07 10:05:57 -0500

    archive data QA and aws push/pull inst scripts. QA now done on database in repel-infrastructure.

--- inst/processing_scripts/push_aws_raw_annual_reports.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-
-# Script to sync data files
-# See https://ecohealthalliance.github.io/eha-ma-handbook/11-cloud-computing-services.html
-# For credentials setup
-
-wahis:::push_aws(folder = "data-raw", filename = "wahis-raw-annual-reports", bucket = "wahis-data")
