703a36e609388e1c391a07cd4c8b729c23918aca
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2020-03-18 16:30:52 -0400

    date handling (partially addressed issue #16)

--- R/transform_outbreak_reports.R
+++ R/transform_outbreak_reports.R
@@ -3,6 +3,7 @@
 #' @import dplyr tidyr purrr
 #' @importFrom janitor clean_names
 #' @importFrom stringr str_detect str_extract
+#' @importFrom lubridate dmy
 #' @export
 
 transform_outbreak_reports <- function(outbreak_reports) {
@@ -40,6 +41,24 @@ transform_outbreak_reports <- function(outbreak_reports) {
   outbreak_reports_events <- outbreak_reports_events %>%
     mutate(new_outbreak_in_report = map_lgl(outbreak_reports2, ~length(.$outbreak_summary) > 1))
   
+  # Dates handling
+  outbreak_reports_events <- outbreak_reports_events %>%
+    mutate_at(vars(starts_with("date"), "report_date", -"date_of_previous_occurrence"), ~dmy(.)) %>% 
+    mutate(date_of_previous_occurrence_nchar = as.character(nchar(date_of_previous_occurrence))) %>% 
+    mutate(date_of_previous_occurrence_resolution = recode(date_of_previous_occurrence_nchar,
+                                                           "4" = "yyyy",
+                                                           "7" = "yyyy-mm",
+                                                           "10" = "yyyy-mm-dd",
+                                                           .default = NA_character_)) %>% 
+    mutate(date_of_previous_occurrence_resolution2 = date_of_previous_occurrence_resolution) %>% 
+    pivot_wider(names_from = date_of_previous_occurrence_resolution2, values_from = date_of_previous_occurrence) %>% 
+    mutate(`yyyy-mm-dd`= dmy(`yyyy-mm-dd`)) %>% 
+    mutate(`yyyy-mm` = myd(`yyyy-mm`, truncated = 1)) %>% 
+    mutate(`yyyy` = ymd(`yyyy`, truncated = 2)) %>% 
+    mutate(date_of_previous_occurrence = coalesce(`yyyy-mm-dd`,`yyyy-mm`, `yyyy`)) %>% 
+    select(-`NA`, -date_of_previous_occurrence_nchar, -`yyyy-mm-dd`,-`yyyy-mm`, -`yyyy`) 
+    
+
   # Outbreaks ---------------------------------------------------
   
   outbreak_reports_detail <- map_df(outbreak_reports2, function(x){
@@ -65,24 +84,24 @@ transform_outbreak_reports <- function(outbreak_reports) {
              total_case_fatality_perc = total_apparent_case_fatality_rate,
              total_susceptible_animals_lost_perc = total_proportion_susceptible_animals_lost)
   }
-      
-    
-    # Laboratories table ---------------------------------------------------
-    outbreak_reports_laboratories <- map_dfr(outbreak_reports2, function(x){
-      tests <- x$diagnostic_tests 
-      if(is.null(dim(tests))){
-        return()
-      }
-      return(tests)
-    }) %>%
-      janitor::clean_names() 
-    
-    # Export -----------------------------------------------
-    wahis_joined <- list("outbreak_reports_events" = outbreak_reports_events, 
-                         "outbreak_reports_outbreaks" = outbreak_reports_detail, 
-                         "outbreak_reports_outbreaks_summary" = outbreak_reports_summary,
-                         "outbreak_reports_laboratories" = outbreak_reports_laboratories)
-    return(wahis_joined)
-    
+  
+  
+  # Laboratories table ---------------------------------------------------
+  outbreak_reports_laboratories <- map_dfr(outbreak_reports2, function(x){
+    tests <- x$diagnostic_tests 
+    if(is.null(dim(tests))){
+      return()
+    }
+    return(tests)
+  }) %>%
+    janitor::clean_names() 
+  
+  # Export -----------------------------------------------
+  wahis_joined <- list("outbreak_reports_events" = outbreak_reports_events, 
+                       "outbreak_reports_outbreaks" = outbreak_reports_detail, 
+                       "outbreak_reports_outbreaks_summary" = outbreak_reports_summary,
+                       "outbreak_reports_laboratories" = outbreak_reports_laboratories)
+  return(wahis_joined)
+  
 }
 
