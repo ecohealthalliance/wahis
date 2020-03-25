27577570cd457517ba8cc5a4c0aee299d667d467
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2020-03-25 16:43:18 -0400

    annotate and clean disease lookup routine

--- inst/processing_scripts/clean_disease_names.R
+++ inst/processing_scripts/clean_disease_names.R
@@ -1,27 +1,26 @@
 # export a single document of disease names for standardization
 library(tidyverse)
 
-
-tmp_destination_file <- paste0(destination_folder, "/ando_tmp.csv.gz")
+# Download ANDO ontology --------------------------------------------------
+tmp_destination_file <- "inst/diseases/ando_tmp.csv.gz"
 
 download.file("http://data.agroportal.lirmm.fr/ontologies/ANDO/download?apikey=1de0a270-29c5-4dda-b043-7c3580628cd5&download_format=csv", destfile = tmp_destination_file)
 
 ando <- read_csv(tmp_destination_file)
+
 ando <- ando %>% 
     select("Class ID", "Preferred Label", "Synonyms", "Obsolete", "Parents" ) %>% 
     janitor::clean_names()
 
-distination_file <- paste0(destination_folder, "/ando_ontology.csv")
-write_csv(ando, distination_file)
-message(paste("ANDO ontology downloaded to", distination_file))
+write_csv(ando, "inst/diseases/ando_ontology.csv")
 fs::file_delete(tmp_destination_file)
 
+# Read in diseases to look up from annual and outbreak reports ------------
 annual_animal <- read_csv(system.file("diseases", "annual_report_diseases_animals.csv", package = "wahis"))
 annual_human <- read_csv(system.file("diseases", "annual_report_diseases_humans.csv", package = "wahis"))
 outbreak_animal <- read_csv(system.file("diseases", "outbreak_report_diseases.csv", package = "wahis"))
 
 diseases_for_lookup <- annual_animal %>% 
-    select(disease_clean) %>%
     mutate(report = "animal") %>% 
     bind_rows(annual_human %>% mutate(report = "annual human")) %>% 
     bind_rows(outbreak_animal %>% 
@@ -31,7 +30,8 @@ diseases_for_lookup <- annual_animal %>%
     arrange(disease_clean) %>% 
     rename(disease = disease_clean)
 
-# get exact matches
+
+# Pre-process ANDO ontology for manual lookups ----------------------------
 ando <-  read_csv(system.file("diseases", "ando_ontology.csv", package = "wahis")) %>% 
     mutate_all(~tolower(.)) %>% 
     mutate(id = basename(class_id)) %>% 
@@ -51,6 +51,7 @@ write_csv(ando, here::here("inst", "diseases", "ando_lookup.csv"))
 # heirarchy here
 # http://agroportal.lirmm.fr/ontologies/ANDO/?p=classes&conceptid=http%3A%2F%2Fopendata.inra.fr%2FAnimalDiseasesOnto%2FAG401
 
+# Do a join to get exact matches ------------------------------------------
 diseases_for_lookup <- diseases_for_lookup %>% 
     left_join(ando %>% distinct(id, preferred_label), by = c("disease" = "preferred_label")) %>%
     left_join(ando %>% distinct(id, synonyms) %>% drop_na(), by = c("disease" = "synonyms")) %>% 
@@ -60,12 +61,4 @@ diseases_for_lookup <- diseases_for_lookup %>%
     distinct()
 
 write_csv(diseases_for_lookup, here::here("inst", "diseases", "disease_lookup.csv"))
-
-
-# diseases <- readxl::read_excel(here::here("inst", "diseases", "disease_lookup.xlsx"))
-#    
-# diseases <- diseases %>% 
-#     left_join(ando %>% distinct(id, preferred_label,class_desc), by = c("ando_id" = "id"))
-
-
-
+# NOTE - manual lookup is done in disease_lookup.xlsx to prevent accidentally overwriting
