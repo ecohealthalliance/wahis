70bf4e047bb6d873d46920e595356d3f18ff132d
emmamendelsohn <emma.mendelsohn@gmail.com>
2021-02-02 18:14:49 -0500

    updating code to work with latest version for tradestatistics package

--- R/connect_ots_trade.R
+++ R/connect_ots_trade.R
@@ -2,16 +2,20 @@
 #' @param directory where ots-trade.rds is saved
 #' @import dplyr here tradestatistics
 #' @importFrom readr write_rds
+#' @importFrom jsonlite fromJSON
 #' @export
 download_trade <- function(directory){
   
-  product_ids <- full_join(tradestatistics::ots_products, tradestatistics::ots_communities) %>%
-    filter(community_name %in% c("Foodstuffs","Animal and Vegetable Bi-Products", "Vegetable Products", "Animal Hides",  "Animal Products")) %>%
+  product_ids <- left_join(tradestatistics::ots_products, tradestatistics::ots_sections, by = "product_code") %>%
+    left_join(tradestatistics::ots_sections_shortnames, by = "section_code") %>% 
+    filter(section_shortname_english %in% c("Foodstuffs","Animal and Vegetable Bi-Products", "Vegetable Products", "Animal Hides",  "Animal Products")) %>%
     pull(product_code)
   
   suppressWarnings(dir.create(here("data-raw")))
-  ots <- ots_create_tidy_data(years = c(2000:2017), table = "yrpc",  products = product_ids,
-                              include_communities = TRUE, include_shortnames = TRUE) 
+  
+  max_year <- fromJSON("https://api.tradestatistics.io/year_range")[2,]
+  
+  ots <- ots_create_tidy_data(years = c(2000:max_year), table = "yrpc",  products = product_ids) 
   
   write_rds(ots, here(directory, "ots-trade.rds"))
 }
