505891ada1959a09f0e822f32115e1c18afc9f78
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-06-17 20:49:43 +0000

    add absent diseases

--- R/transform_six_month_reports.R
+++ R/transform_six_month_reports.R
@@ -84,147 +84,153 @@ transform_six_month_reports <- function(six_month_reports) {
     # initializationDto$isAquatic
     # initializationDto$areaId
     
-    # disease present ---------------------------------------------------------
     tic()
-    diseases_present <- imap(six_month_reports2[1:200], function(x, y){
+    transformed_reports <- imap(six_month_reports2[1:200], function(x, y){
         
         print(y)
         
+        # diseases present ---------------------------------------------------------
+        
+       out <- map(c("occCodePresentDiseaseList", "occCodeAbsentDiseaseList"), function(xx){
+            
+            dat <- x[[xx]] %>% 
+                as_tibble() 
+            
+            if(nrow(dat) == 0) return()
+            
+            # top level info
+            dat <-  dat %>% 
+                mutate(report_id = x$reportId,
+                       report_country =  na.omit(x$reportInfoDto$reportUserInfoDtoList$countryOrTerritory),
+                       report_semester = x$initializationDto$period$period,
+                       report_year = x$initializationDto$period$year,
+                       isAquatic = x$initializationDto$animalTypeDto$isAquatic,
+                       areaID = x$initializationDto$areaDto$areaId,
+                       oieReference = x$oieReference) %>% 
+                relocate(suppressWarnings(one_of("reportDiseaseDtoList")), .after = everything())
+            
+            dat <- dat %>%    
+                flex_unnest("reportDiseaseDtoList")
+            
+            # diseaseDto
+            dat <- dat %>% 
+                mutate(diseaseName = .$diseaseDto$diseaseName) %>% 
+                flex_unselect("diseaseDto")
+            
+            # templatePreviewDto
+            dat <- dat %>% 
+                mutate(templatePreviewDto = templatePreviewDto[[1]]) %>% 
+                flex_unnest("templatePreviewDto") %>% 
+                flex_unselect("templatePreviewDto") %>% 
+                flex_bind_cols(flex_pull(., "periodDto")) %>% 
+                flex_unselect("periodDto") %>% 
+                flex_unnest("diseaseTypeDto") %>% 
+                flex_unnest("areaSpeciesDto") %>% 
+                flex_bind_cols(flex_pull(., "diseaseTypeCombDto")) %>% 
+                flex_unselect("diseaseTypeCombDto")  %>% 
+                select(-ends_with("List")) %>% 
+                flex_bind_cols(flex_pull(., "qtySummariesDto")) %>% 
+                flex_unselect("qtySummariesDto")
+            
+            # reportOccCmDto
+            dat <- dat %>% 
+                flex_bind_cols(flex_pull(., "reportOccCmDto")) %>% 
+                flex_unselect("reportOccCmDto") %>% 
+                flex_unnest("occSummariesList") %>% 
+                flex_bind_cols(flex_pull(., "occurrenceCodeDto")) %>% 
+                flex_unselect("occurrenceCodeDto")
+            
+            # check mismatches (dont worry about nature, status, newOutbreak, totalOutbreak because they are being excluded)
+            assert_distinct(dat, "areaName")
+            areaname <- colnames(dat)[str_detect(colnames(dat), "areaName")]
+            if(length(areaname) > 1) { 
+                areaname_remove <- areaname[-1]
+                dat <- dat %>% 
+                    select(-all_of(areaname_remove) )
+                
+                if(length(areaname) > 0){
+                    dat <- dat %>% 
+                        rename(areaName = areaname[[1]]) 
+                }
+            }
+            
+            # quant data select
+            quant <- dat %>% 
+                select(
+                    report_country, # country
+                    report_id,# report
+                    report_semester, # report semester
+                    report_year, # report year
+                    isAquatic,
+                    areaID,
+                    oieReference,
+                    # starts_with("isWild"), # lots of inconsistencies, eg "https://wahis.oie.int/smr/pi/report/24848?format=preview"
+                    flex_one_of("animalCategory"),
+                    flex_one_of("diseaseName"), # disease
+                    flex_one_of("isAbsenceOfOutbreak"), # disease_status
+                    flex_one_of("animalCategoryDomestic"),
+                    flex_one_of("animalCategoryWild"),
+                    flex_one_of("code"),
+                    flex_one_of("specieName"), # taxa
+                    flex_one_of("subPeriodTrans"), # period
+                    flex_one_of("areaName"),  # adm
+                    flex_one_of("templateName"), # adm type
+                    flex_one_of("susceptible"), # susceptible_detail
+                    flex_one_of("quantitiesNcase"), # cases_detail
+                    flex_one_of("quantitiesDead"), # deaths_detail
+                    flex_one_of("killedAndDisplosed"), # killed_and_disposed_of_detail
+                    flex_one_of("slaughtered"), # slaughtered_detail
+                    flex_one_of("vaccinated"),
+                    flex_one_of("transDiseaseType")  # serotype
+                    #  flex_one_of("quantatiesUnit") # measuring_units
+                    # flex_one_of("measure") # control measures
+                    #flex_one_of("vaccNb") # vaccination_in_response_to_the_outbreak_detail
+                )
+            
+            # control measures separately - disease and report specific
+            cm <- dat %>%
+                select(
+                    report_country,
+                    report_id,# report
+                    report_semester, # report semester
+                    report_year, # report year
+                    isAquatic,
+                    areaID,
+                    oieReference,
+                    diseaseName,
+                    flex_one_of('cmSummariesList')) %>% 
+                distinct() %>% 
+                flex_unnest('cmSummariesList') %>% 
+                flex_unnest("speciesCmList") %>% 
+                flex_unnest("cmList") %>% 
+                select(
+                    -flex_one_of("cmId"),
+                    -flex_one_of("nature"),
+                    -flex_one_of("status"),
+                    -flex_one_of("smrCmdId"),
+                    -flex_one_of("cmKey"),
+                    -flex_one_of("specieId")
+                )
+            
+            
+            return(list("quant" = quant, "control_measures" = cm))
+            
+        }) %>% 
+           set_names(c("present", "absent"))
+       
        
         
-        dat <- x$occCodePresentDiseaseList %>% 
-            as_tibble() 
-        
-        if(nrow(dat) == 0) return()
-        
-        # top level info
-        dat <-  dat %>% 
-            mutate(report_id = x$reportId,
-                   report_country =  na.omit(x$reportInfoDto$reportUserInfoDtoList$countryOrTerritory),
-                   report_semester = x$initializationDto$period$period,
-                   report_year = x$initializationDto$period$year,
-                   isAquatic = x$initializationDto$animalTypeDto$isAquatic,
-                   areaID = x$initializationDto$areaDto$areaId,
-                   oieReference = x$oieReference) %>% 
-            relocate(suppressWarnings(one_of("reportDiseaseDtoList")), .after = everything())
-        
-        dat <- dat %>%    
-            flex_unnest("reportDiseaseDtoList")
-        
-        # diseaseDto
-        dat <- dat %>% 
-            mutate(diseaseName = .$diseaseDto$diseaseName) %>% 
-            flex_unselect("diseaseDto")
-        
-        # templatePreviewDto
-        dat <- dat %>% 
-            mutate(templatePreviewDto = templatePreviewDto[[1]]) %>% 
-            flex_unnest("templatePreviewDto") %>% 
-            flex_unselect("templatePreviewDto") %>% 
-            flex_bind_cols(flex_pull(., "periodDto")) %>% 
-            flex_unselect("periodDto") %>% 
-            flex_unnest("diseaseTypeDto") %>% 
-            flex_unnest("areaSpeciesDto") %>% 
-            flex_bind_cols(flex_pull(., "diseaseTypeCombDto")) %>% 
-            flex_unselect("diseaseTypeCombDto")  %>% 
-            select(-ends_with("List")) %>% 
-            flex_bind_cols(flex_pull(., "qtySummariesDto")) %>% 
-            flex_unselect("qtySummariesDto")
-        
-        # reportOccCmDto
-        dat <- dat %>% 
-            flex_bind_cols(flex_pull(., "reportOccCmDto")) %>% 
-            flex_unselect("reportOccCmDto") %>% 
-            flex_unnest("occSummariesList") %>% 
-            flex_unnest("cmSummariesList") %>% 
-            flex_bind_cols(flex_pull(., "occurrenceCodeDto")) %>% 
-            flex_unselect("occurrenceCodeDto")
-        
-        # check mismatches (dont worry about nature, status, newOutbreak, totalOutbreak because they are being excluded)
-        assert_distinct(dat, "areaName")
-        areaname <- colnames(dat)[str_detect(colnames(dat), "areaName")]
-        if(length(areaname) > 1) { 
-            areaname_remove <- areaname[-1]
-            dat <- dat %>% 
-                select(-all_of(areaname_remove) )
-            
-            if(length(areaname) > 0){
-                dat <- dat %>% 
-                    rename(areaName = areaname[[1]]) 
-            }
-        }
-        
-        # quant data select
-        quant <- dat %>% 
-            select(
-                report_country, # country
-                report_id,# report
-                report_semester, # report semester
-                report_year, # report year
-                isAquatic,
-                areaID,
-                oieReference,
-                # starts_with("isWild"), # lots of inconsistencies, eg "https://wahis.oie.int/smr/pi/report/24848?format=preview"
-                flex_one_of("animalCategory"),
-                flex_one_of("diseaseName"), # disease
-                flex_one_of("isAbsenceOfOutbreak"), # disease_status
-                flex_one_of("animalCategoryDomestic"),
-                flex_one_of("animalCategoryWild"),
-                flex_one_of("code"),
-                flex_one_of("specieName"), # taxa
-                flex_one_of("subPeriodTrans"), # period
-                flex_one_of("areaName"),  # adm
-                flex_one_of("templateName"), # adm type
-                flex_one_of("susceptible"), # susceptible_detail
-                flex_one_of("quantitiesNcase"), # cases_detail
-                flex_one_of("quantitiesDead"), # deaths_detail
-                flex_one_of("killedAndDisplosed"), # killed_and_disposed_of_detail
-                flex_one_of("slaughtered"), # slaughtered_detail
-                flex_one_of("vaccinated"),
-                flex_one_of("transDiseaseType"),  # serotype
-                    flex_one_of("speciesCmList")
-                #  flex_one_of("quantatiesUnit") # measuring_units
-                # flex_one_of("measure") # control measures
-                #flex_one_of("vaccNb") # vaccination_in_response_to_the_outbreak_detail
-            )
-        
-        # control measures separately - disease and report specific
-        cm <- dat %>%
-            select(
-                report_country,
-                report_id,# report
-                report_semester, # report semester
-                report_year, # report year
-                isAquatic,
-                areaID,
-                oieReference,
-                diseaseName,
-                flex_one_of('speciesCmList')) %>% 
-            distinct() %>% 
-            flex_unnest("speciesCmList") %>% 
-            flex_unnest("cmList") %>% 
-            select(
-               -flex_one_of("cmId"),
-               -flex_one_of("nature"),
-               -flex_one_of("status"),
-               -flex_one_of("smrCmdId"),
-               -flex_one_of("cmKey"),
-               -flex_one_of("specieId")
-            )
-        
-        # map_chr(dat, class)
-        
-        return(list(quant, cm))
-        
     })
+    
     toc()
     
-    diseases_present <- compact(diseases_present)
+    transformed_reports <- compact(modify_depth(transformed_reports, 1, compact))
     
-    #TODO post process, wild or not
-    #TODO add: iso3c, ando_id
     #TODO add absence, unreported
-    #TODO make yearly summary?
+    
+    #TODO post process:  wild or not, iso3c, ando_id
+    
+    #TODO make semester summary?
     
 }
 
