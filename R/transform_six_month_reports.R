c01fa9842995d79d882b804248ef53cc414e7ffa
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-06-17 15:15:33 +0000

    a bunch of flex functions to make selecting and unnesting robust

--- R/transform_six_month_reports.R
+++ R/transform_six_month_reports.R
@@ -1,13 +1,26 @@
-replace_x <- function(x, replacement = NA_character_) {
-    if (length(x) == 0 || length(x[[1]]) == 0) {
-        replacement
-    } else {
-        x
-    }
+flex_unselect <- function(df, x){
+    suppressWarnings(select(.data = df, -one_of(x)))
 }
 
-one_of_sw <- function(x){
-    suppressWarnings(one_of(x))
+flex_pull <- function(df, x){
+   if(x %in% colnames(df)){
+      return(pull(df, x))
+   }else{
+       return(NULL)
+   }
+}
+
+flex_bind_cols <- function(df1, df2){
+    suppressMessages(bind_cols(df1, df2))
+}
+
+flex_unnest <- function(df, x){
+    out <- suppressMessages(suppressWarnings(unnest(data = df, cols = one_of(x), names_repair = "universal")))
+    if(nrow(out)==0){
+        return(df)
+    }else{
+        return(out)
+    }
 }
 
 
@@ -63,7 +76,10 @@ transform_six_month_reports <- function(six_month_reports) {
     
     # library(profvis)
     # profvis({
-    diseases_present <- map(six_month_reports2[1:20], function(x){
+    tic()
+    diseases_present <- imap(six_month_reports2[1:200], function(x, y){
+        
+        print(y)
         
         dat <- x$occCodePresentDiseaseList %>% 
             as_tibble() 
@@ -71,122 +87,91 @@ transform_six_month_reports <- function(six_month_reports) {
         if(nrow(dat) == 0) return()
         
         dat <- dat %>%    
-            unnest(reportDiseaseDtoList)
+            flex_unnest("reportDiseaseDtoList")
         
-        tic()
         # diseaseDto
         out <- dat %>% 
             mutate(diseaseName = .$diseaseDto$diseaseName) %>% 
-            select(-diseaseDto)
+            flex_unselect("diseaseDto")
         
         # templatePreviewDto
         out <- out %>% 
             mutate(templatePreviewDto = templatePreviewDto[[1]]) %>% 
-            unnest(templatePreviewDto) %>% 
-            select(-templatePreviewDto) %>% 
-            bind_cols(pull(., periodDto)) %>% 
-            select(-periodDto) %>% 
-            unnest(diseaseTypeDto) %>% 
-            unnest(areaSpeciesDto) %>% 
-            bind_cols(pull(., diseaseTypeCombDto)) %>% 
-            select(-diseaseTypeCombDto)  %>% 
-            select(-ends_with("List"))
-        
-        out$qtySummariesDto <- out$qtySummariesDto %>% 
-            select(-intersect(names(out), names(.)))
-        
-        out <- out %>% 
-            bind_cols(pull(., qtySummariesDto)) %>% 
-            select(-qtySummariesDto) 
+            flex_unnest("templatePreviewDto") %>% 
+            flex_unselect("templatePreviewDto") %>% 
+            flex_bind_cols(flex_pull(., "periodDto")) %>% 
+            flex_unselect("periodDto") %>% 
+            flex_unnest("diseaseTypeDto") %>% 
+            flex_unnest("areaSpeciesDto") %>% 
+            flex_bind_cols(flex_pull(., "diseaseTypeCombDto")) %>% 
+            flex_unselect("diseaseTypeCombDto")  %>% 
+            select(-ends_with("List")) %>% 
+            flex_bind_cols(flex_pull(., "qtySummariesDto")) %>% 
+            flex_unselect("qtySummariesDto")
         
         # reportOccCmDto
         out <- out %>% 
-            bind_cols(pull(., reportOccCmDto)) %>% 
-            select(-reportOccCmDto)
+            flex_bind_cols(flex_pull(., "reportOccCmDto")) %>% 
+            flex_unselect("reportOccCmDto") %>% 
+            flex_unnest("occSummariesList") %>% 
+            flex_unnest("cmSummariesList") %>% 
+            flex_bind_cols(flex_pull(., "occurrenceCodeDto")) %>% 
+            flex_unselect("occurrenceCodeDto") %>% 
+            flex_unnest("speciesCmList") %>% 
+            flex_unnest("cmList")
         
-        out$occSummariesList <- out$occSummariesList %>% 
-            map(., ~select(., -isWild))
-        
-        out$cmSummariesList <- out$cmSummariesList %>% 
-            map(., ~select(., -isWild))
-        
-
-        out <- out %>% 
-            unnest(occSummariesList, names_repair = "universal") %>% 
-            unnest(cmSummariesList, names_repair = "universal")
-        
-        out$occurrenceCodeDto <-  out$occurrenceCodeDto %>% 
-            select(-intersect(names(out), names(.)))
-        
-        out$speciesCmList <- out$speciesCmList %>% 
-            map(., ~select(., -specieId, -specieName))
-
-        
-        out <- out %>% 
-            bind_cols(pull(., occurrenceCodeDto)) %>% 
-            select(-occurrenceCodeDto) %>% 
-            unnest(speciesCmList, names_repair = "universal") 
-        
-        out$cmList <- out$cmList %>% 
-            map(., ~select(., -nature, -status))
-        
-        out <- out %>% 
-            unnest(cmList, names_repair = "universal")
-        
-            toc()
-            
-       # map_chr(out, class)
-        
-
-        # out <- out %>% 
-        #     mutate(report_id = x$reportId,
-        #            period = x$initializationDto$period$period,
-        #            year = x$initializationDto$period$year,
-        #            isAquatic = x$initializationDto$animalTypeDto$isAquatic,
-        #            areaID = x$initializationDto$areaDto$areaId,
-        #            oieReference = x$oieReference) %>% 
-        #     select(
-        #         # country
-        #         report_id,# report
-        #         period, # report semester
-        #         year, # report year
-        #         isAquatic,
-        #         areaID,
-        #         oieReference,
-        #         one_of_sw("diseaseName"), # disease
-        #         starts_with("isWild"),# disease population
-        #         one_of_sw("isAbsenceOfOutbreak"), # disease_status
-        #         one_of_sw("animalCategoryDomestic"),
-        #         one_of_sw("animalCategoryWild"),
-        #         one_of_sw("code"),
-        #         starts_with("specieName"), # taxa
-        #         one_of_sw("subPeriodTrans"), # period
-        #         # temporal_scale
-        #         starts_with("areaName"),  # adm
-        #         one_of_sw("templateName"), # adm type
-        #         one_of_sw("susceptible"), # susceptible_detail
-        #         one_of_sw("quantitiesNcase"), # cases_detail
-        #         one_of_sw("quantitiesDead"), # deaths_detail
-        #         one_of_sw("killedAndDisplosed"), # killed_and_disposed_of_detail
-        #         one_of_sw("slaughtered"), # slaughtered_detail
-        #         one_of_sw("vaccNb"), # vaccination_in_response_to_the_outbreak_detail
-        #         one_of_sw("vaccinated"),
-        #         # serotype
-        #         # measuring_units
-        #         one_of_sw("measure") # control measures
-        #     )
+        # map_chr(out, class)
         
         return(out)
         
     })
+    toc()
     
-    #TODO profiling speed up - pull things out directly - in progress - need to make all selections robust and handle dup names
     #TODO dupe name handling needs to be post processed -- too slow dealt with upfront , should allow universal fix then just select first occurance
     #TODO field selections (commented out)
     #TODO get countryname
     #TODO add: iso3c, ando_id
     #TODO summarize over control measures, separate out detail and summary
     #TODO add absence, unreported
+    
+    # out <- out %>% 
+    #     mutate(report_id = x$reportId,
+    #            period = x$initializationDto$period$period,
+    #            year = x$initializationDto$period$year,
+    #            isAquatic = x$initializationDto$animalTypeDto$isAquatic,
+    #            areaID = x$initializationDto$areaDto$areaId,
+    #            oieReference = x$oieReference) %>% 
+    #     select(
+    #         # country
+    #         report_id,# report
+    #         period, # report semester
+    #         year, # report year
+    #         isAquatic,
+    #         areaID,
+    #         oieReference,
+    #         one_of_sw("diseaseName"), # disease
+    #         starts_with("isWild"),# disease population
+    #         one_of_sw("isAbsenceOfOutbreak"), # disease_status
+    #         one_of_sw("animalCategoryDomestic"),
+    #         one_of_sw("animalCategoryWild"),
+    #         one_of_sw("code"),
+    #         starts_with("specieName"), # taxa
+    #         one_of_sw("subPeriodTrans"), # period
+    #         # temporal_scale
+    #         starts_with("areaName"),  # adm
+    #         one_of_sw("templateName"), # adm type
+    #         one_of_sw("susceptible"), # susceptible_detail
+    #         one_of_sw("quantitiesNcase"), # cases_detail
+    #         one_of_sw("quantitiesDead"), # deaths_detail
+    #         one_of_sw("killedAndDisplosed"), # killed_and_disposed_of_detail
+    #         one_of_sw("slaughtered"), # slaughtered_detail
+    #         one_of_sw("vaccNb"), # vaccination_in_response_to_the_outbreak_detail
+    #         one_of_sw("vaccinated"),
+    #         # serotype
+    #         # measuring_units
+    #         one_of_sw("measure") # control measures
+    #     )
+    
     
     #  })
 }
