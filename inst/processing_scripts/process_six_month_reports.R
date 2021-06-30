aa700370a99862204e537d48a83b52f4c8c6f992
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-06-30 20:45:22 +0000

    updated processing workflow

--- inst/processing_scripts/process_six_month_reports.R
+++ inst/processing_scripts/process_six_month_reports.R
@@ -3,29 +3,30 @@ library(scrapetools)
 library(tictoc)
 
 # # get 6 month reports list -----------------------------------------------
-# report_list <- scrape_six_month_report_list()
-# 
-# reports_to_get <- report_list %>% 
-#     select(report_id) %>% 
-#     mutate(url = paste0("https://wahis.oie.int/smr/pi/report/", report_id, "?format=preview"))
-# 
-# # Pulling reports ----------------------------
-# message("Pulling ", nrow(reports_to_get), " reports")
-# 
-# tic()
-# report_resps <- split(reports_to_get, (1:nrow(reports_to_get)-1) %/% 100) %>% # batching by 100s
-#     map(function(reports_to_get_split){
-#         map_curl(
-#             urls = reports_to_get_split$url,
-#             .f = function(x) wahis::safe_ingest(x),
-#             .host_con = 8L,
-#             .delay = 0.5,
-#             .handle_opts = list(low_speed_limit = 100, low_speed_time = 300), # bytes/sec
-#             .retry = 2,
-#             .handle_headers = list(`Accept-Language` = "en")
-#         )
-#     })
-# toc()
+report_list <- scrape_six_month_report_list()
+
+reports_to_get <- report_list %>%
+    select(report_id) %>%
+    mutate(url = paste0("https://wahis.oie.int/smr/pi/report/", report_id, "?format=preview"))
+
+# Pulling reports ----------------------------
+message("Pulling ", nrow(reports_to_get), " reports")
+
+tic()
+report_resps <- split(reports_to_get, (1:nrow(reports_to_get)-1) %/% 100) %>% # batching by 100s
+    map(function(reports_to_get_split){
+        map_curl(
+            urls = reports_to_get_split$url,
+            .f = function(x) wahis::safe_ingest(x),
+            .host_con = 8L,
+            .delay = 0.5,
+            .handle_opts = list(low_speed_limit = 100, low_speed_time = 300), # bytes/sec
+            .retry = 2,
+            .handle_headers = list(`Accept-Language` = "en")
+        )
+    })
+toc()
+
 # Save ingested files   ------------------------------------------------------
 # dir_create(here::here("data-processed"))
 # readr::write_rds(report_resps, here::here("data-processed", "report_resps_six_month.rds"), compress = "xz", compression = 9L)
@@ -42,3 +43,9 @@ six_month_report_tables <- reduce(six_month_report_tables, c)
 readr::write_rds(six_month_report_tables, here::here("data-processed", "transformed_six_month.rds"), compress = "xz", compression = 9L)
 toc()
 
+six_month_report_tables <- readr::read_rds(here::here("data-processed", "transformed_six_month.rds"))
+
+quantitative_reports_summary <- reduce(six_month_report_tables[names(six_month_report_tables)== "quantitative_reports_summary" ],
+                                       bind_rows)
+
+readr::write_rds(quantitative_reports_summary, here::here("data-processed", "transformed_six_month_quantitative_reports_summary.rds"), compress = "xz", compression = 9L)
