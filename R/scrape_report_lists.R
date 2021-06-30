21dfdf390a0c6d081b52db6c05e420a308bde8f6
emmamendelsohn <mendelsohn@ecohealthalliance.org>
2021-06-30 18:26:39 +0000

    fix report list scraper following wahis website updates

--- R/scrape_report_lists.R
+++ R/scrape_report_lists.R
@@ -1,4 +1,4 @@
-#' Generic funtion to get master list of reports
+#' Generic function to get master list of reports
 #' Works for outbreak and six-month reports
 #' @return A tibble of reports
 #' @noRd
@@ -6,43 +6,48 @@
 #' @importFrom janitor clean_names
 #' @importFrom httr POST content
 #' @importFrom assertthat assert_that
-scrape_report_list <- function(post_url, start_date = "2000-01-01", end_date = Sys.Date()) {
-
-page_size <- 1000000L
-
-body_data <-
-    list(
-        pageNumber = 1L,
-        pageSize = page_size,
-        searchText = "",
-        sortColName = "",
-        sortColOrder = "ASC",
-        reportFilters = list(reportDate = list(
-            startDate = start_date, endDate = end_date
-        )),
-        languageChanged = FALSE
-    )
-
-report_list_response <- POST(
-    post_url,
-    body = body_data,
-    encode = "json")
-
-report_list <- content(report_list_response)[[2]]
-
-assertthat::assert_that(length(report_list) < page_size) # otherwise you are not retrieving all the results
-
-reports <- map_dfr(report_list, as_tibble) %>% 
-    janitor::clean_names()
-
-return(reports)
+scrape_report_list <- function(post_url) {
+    
+    page_size <- 1000000L
+    # start_date <- "2000-01-01"
+    # end_date = Sys.Date()
+    
+    body_data <-
+        list(
+            pageNumber = 1L,
+            pageSize = page_size,
+            searchText = "",
+            sortColName = "",
+            sortColOrder = "ASC",
+            # reportFilters = list(reportDate = list(
+            #     startDate = start_date, endDate = end_date
+            # )),
+            languageChanged = FALSE )
+    
+    
+    report_list_response <- POST(
+        post_url,
+        body = body_data,
+        # add_headers('token' = "#PIPRD202006#",
+        #             'accept-language' = "en"),
+        encode = "json")
+    
+    
+    report_list <- content(report_list_response)[[2]]
+    
+    assertthat::assert_that(length(report_list) < page_size) # otherwise you are not retrieving all the results
+    
+    reports <- map_dfr(report_list, as_tibble) %>% 
+        janitor::clean_names()
+    
+    return(reports)
 }
 
 #' Get master list of outbreak reports
 #' @return A tibble of reports
 #' @export
 scrape_outbreak_report_list <- function() {
-   scrape_report_list("https://wahis.oie.int/pi/getReportList")
+    scrape_report_list("https://wahis.oie.int/pi/getReportList")
 }
 
 
